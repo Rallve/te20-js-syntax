9110227f702fb5c7d3cf64508a62dbbadb59a4e1
Rallve <77719378+Rallve@users.noreply.github.com>
2022-09-20 13:58:56 +0200

    ✨ Gjorde leos kod

--- leos.js
+++ leos.js
@@ -10,3 +10,17 @@
 // }
 
 // ni behöver 2 inmatningar och sedan en uträkning och en utskrift
+
+const sida1 = document.querySelector('#input-field1');
+const sida2 = document.querySelector('#input-field2');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    const sid1 = sida1.value;
+    const sid2 = sida2.value;
+    const hypo = Math.sqrt(sid1 * sid1 + sid2 * sid2);
+    const messageBox = document.querySelector('#message-box');
+    let message = document.createElement('p');
+    message.textContent = `Hypotenusan är ${hypo} cm`;
+    messageBox.appendChild(message);
+});
